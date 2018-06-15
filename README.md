93f92e86ed247b25cd48854937d0a173730d8b8f
TangAnna <atang@singlemountaintech.com>
2018-06-15 17:48:49 +0800

    更新README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # WebViewInteractiveAndroid
 Android使用WebView实现与js交互
+现在很多App中使用了Web的方式实现其中的一些功能，这样的开发模式避免不了需要Android和Web要进行数据交互，
+在Android中提供了WebView控件来完成加载web页面的加载，也能通过它来实现两者之间的数据交互；<br/>
+面试中也会被问到这方面的知识，所以学习Android和web的交互是开发人员必不可少的一项技能；<br/>
+
+首先是WebView经常用到的一些方法：<br/>
+//TODO :下周总结
+
+**Js调用Android的实现方式：**<br/>
+方法一：<br/>
+
+步骤：<br/>
+1.设置webView可以加载Js;<br/>
+2.创建Js接口类；
+3.设置Js接口；<br/>
+4.加载Web页面；<br/>
+
+代码实现如下：
+    
+    //设置webView可以加载Js
+    mWebView.getSettings().setJavaScriptEnabled(true);
+创建Js接口类，创建一个类，根据业务写方法，在该方法上添加注解 "@JavascriptInterface"(注解很重要)
+
+
+    //设置js接口
+    //参数1：创建的js接口类的对象
+    //参数2：js方法中用if判断的值，这两个值要保持一致  if (window.imoocLauncher)
+    mWebView.addJavascriptInterface(new ImoocInterface(this), "imoocInterface");
+    
+设置要加载的网页
+    
+    //我的是本地的html页面路径写法
+     mWebView.loadUrl("file:///android_asset/index.html");
+
+**Android调用Js中的方法：**<br/>
+
+方法一：
--- app/src/main/java/com/example/tanganan/webviewinteractiveandroid/AndroidCallJsActivity.java
+++ app/src/main/java/com/example/tanganan/webviewinteractiveandroid/AndroidCallJsActivity.java
@@ -1,9 +1,13 @@
 package com.example.tanganan.webviewinteractiveandroid;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,6 +35,7 @@ public class AndroidCallJsActivity extends AppCompatActivity implements View.OnC
         mWebView.loadUrl("file:///android_asset/index.html");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -38,10 +43,23 @@ public class AndroidCallJsActivity extends AppCompatActivity implements View.OnC
                 String str = mEditText.getText().toString();
                 if (TextUtils.isEmpty(str)) {
                     Toast.makeText(this, "请输入", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     //调用js中的显示数据的方法
-                    //使用loadUrl()方法，参数是一段js代码 前面拼接javascript: window.后面的是js中的方法名（先判断是否含有此方法在调用）
-                    mWebView.loadUrl("javascript:if(window.androidSetValue){window.androidSetValue('"+str+"')}");
+                    if (Build.VERSION.SDK_INT < 18) {
+                        //第一种方法：使用loadUrl()方法，参数是一段js代码 前面拼接javascript: window.后面的是js中的方法名（先判断是否含有此方法在调用）
+                        mWebView.loadUrl("javascript:if(window.androidSetValue){window.androidSetValue('" + str + "')}");
+                    } else {
+                        //方法二：（只适用于Android4.4及以上）使用evaluateJavascript()方法
+                        //第一个参数与上一种方法一致，第二个参数是获取js返回值得方法
+                        mWebView.evaluateJavascript("javascript:if(window.androidSetValue){window.androidSetValue('" + str + "')}", new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String value) {
+                                //获取js的返回值
+                                Log.d("TAG", "onReceiveValue: " + value);
+                            }
+                        });
+                    }
+
                 }
 
                 break;
